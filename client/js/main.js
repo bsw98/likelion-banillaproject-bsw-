f6e79452f8131318d7ccd468e9f28a7005a435e6
bsw98 <qwqq6954@naver.com>
2023-01-29 20:26:56 +0900

    네비게이션 마크업

--- client/js/main.js
+++ client/js/main.js
@@ -2,6 +2,27 @@ function handler(){
   let AD = document.querySelector('.TopAd');
   AD.style.display = 'none';
 }
-let button = document.querySelector('.AdClose')
+let body = document.querySelector('body')
+let button = document.querySelector('.AdClose');
+let MenuButton = document.querySelector('.MenuButton');
+let MenuButtonList = document.querySelectorAll('.MenuBar li')
+function ShowMenu(e){
+      e = e.target;
+      
+      if(e == button){
+        let AD = document.querySelector('.TopAd');
+        AD.style.display = 'none';
+      }
+      if(e == MenuButton){
+       
+        MenuButtonList.forEach((item)=>{item.style.display = 'block'})
+      }else{
+        
+        MenuButtonList.forEach((item)=>{item.style.display = 'none'})
+      }
+}
 
+
+
+body.addEventListener('click',ShowMenu);
 button.addEventListener('click',handler)
