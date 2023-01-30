e3adbc21ac365cbdc957202a04b5f4bfcccbdca2
bsw98 <qwqq6954@naver.com>
2023-01-30 11:15:24 +0900

    폰트 적용

--- client/js/main.js
+++ client/js/main.js
@@ -34,7 +34,7 @@ button.addEventListener('click',handler);
 const swiper = new Swiper('.swiper', {
   autoplay: true,
   loop: true, // 무한루프로 걸어서 맨 앞으로 후루룩 돌아오는 걸 방지
-  speed: 2000,
+  speed: 1000,
   pagination: {
     el: '.swiper-pagination',
   },
